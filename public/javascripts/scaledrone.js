1bfba3d831b1b6d185ea4277bab5360cb15ed179
PedroHenriqueMartinsPimenta <pedrohenrique234322@gmail.com>
2020-04-27 15:47:37 -0300

    finish frist version 11

--- public/javascripts/scaledrone.js
+++ public/javascripts/scaledrone.js
@@ -34,7 +34,7 @@
     room.on('members', members => {
       console.log('MEMBERS', members);
       // If we are the second user to connect to the room we will be creating the offer
-      const isOfferer = members.length === 100;
+      const isOfferer = members.length >= 2
       startWebRTC(isOfferer);
     });
   });
